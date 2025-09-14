82cd8008cd35331468eb5c17f96ea1f51bf05ba2
MayteLatalladiEst <mayte.latalladi@upr.edu>
2025-09-14 00:26:30 +0000

    TERMINADO

--- minilab03.cpp
+++ minilab03.cpp
@@ -1,6 +1,9 @@
 /*
-Mayté A. Latalladi Oliveras
-801-22-7658
+Asignación #1: Tres números aleatorios
+Nombre: Mayté A. Latalladi Oliveras
+Número de estudiante: 801-22-7658
+Colaboraciones: 
+    Usé las presentaciones de la clase
 */
 
 #include <iostream> 
@@ -8,33 +11,53 @@ Mayté A. Latalladi Oliveras
 
 using namespace std;
 
+//Se crea la función main 
 int main (){
 
 
     srand(time(0));
 
+    //Definimos las variables donde vamos a almacenar los numeros aleatorios 
     int numeroAleatorio1 = rand() % ( 100 - 0 + 1 ) + 0;
     int numeroAleatorio2 = rand() % ( 100 - 0 + 1 ) + 0;
     int numeroAleatorio3 = rand() % ( 100 - 0 + 1 ) + 0;
 
-
+    //Desplegamos los números aleatorios 
     std::cout << "Números aleatorios generados: " << numeroAleatorio1 << ", " << numeroAleatorio2 << ", " << numeroAleatorio3 <<endl;
     
-    if (numeroAleatorio1 >= numeroAleatorio2 && numeroAleatorio3)
-        std::cout << "Números ordenados: " << numeroAleatorio2 << ", " << numeroAleatorio3 << ", " << numeroAleatorio1 << endl; 
-    else if (numeroAleatorio2 >= numeroAleatorio1 && numeroAleatorio3)
-        std::cout << "Números ordenados: " << numeroAleatorio1 << ", " << numeroAleatorio3 << ", " << numeroAleatorio2 << endl; 
-    else if (numeroAleatorio3 >= numeroAleatorio2 && numeroAleatorio1)
-        std::cout << "Números ordenados: " << numeroAleatorio2 << ", " << numeroAleatorio3 << ", " << numeroAleatorio3 << endl; 
-    else if (numeroAleatorio1 <= numeroAleatorio2 && numeroAleatorio3)
-        std::cout << "Números ordenados: " << numeroAleatorio1 << ", " << numeroAleatorio2 << ", " << numeroAleatorio3 << endl; 
-    else if (numeroAleatorio2 <= numeroAleatorio1 && numeroAleatorio3)
-        std::cout << "Números ordenados: " << numeroAleatorio2 << ", " << numeroAleatorio1 << ", " << numeroAleatorio3 << endl; 
-    else if (numeroAleatorio3 <= numeroAleatorio1 && numeroAleatorio2)
-        std::cout << "Números ordenados: " << numeroAleatorio3 << ", " << numeroAleatorio2 << ", " << numeroAleatorio1 << endl; 
+    //Definimos las variables donde vamos a almacenar los números en orden 
+    int primero, segundo, tercero;
+
+        //Encontramos el número menor
+        if (numeroAleatorio1 <= numeroAleatorio2 && numeroAleatorio1 <= numeroAleatorio3) {
+            primero = numeroAleatorio1;
+        }  else if (numeroAleatorio2 <= numeroAleatorio1 && numeroAleatorio2 <= numeroAleatorio3) {
+                primero = numeroAleatorio2;
+        }  else {
+                primero = numeroAleatorio3;
+        }
+
+        //Encontramos el número mayor   
+        if (numeroAleatorio1 >= numeroAleatorio2 && numeroAleatorio1 >= numeroAleatorio3){
+            tercero = numeroAleatorio1;
+        }  else if (numeroAleatorio2 >= numeroAleatorio1 && numeroAleatorio2 >= numeroAleatorio3) {
+                tercero = numeroAleatorio2;
+        }  else {
+                tercero = numeroAleatorio3;
+        }
+
+        //Encontramos el número del medio por eliminación  
+        if ((numeroAleatorio1 != primero) && (numeroAleatorio1 != tercero)) {
+            segundo = numeroAleatorio1;
+        }  else if ((numeroAleatorio2 != primero) && (numeroAleatorio2 != tercero)) {
+                segundo = numeroAleatorio2;
+        }  else {
+                segundo = numeroAleatorio3;
+        }
+
+        //Desplegamos los números en orden.  
+        std::cout << "Números ordenados: " << primero << ", " << segundo << ", " << tercero << endl; 
+
+
 }
-
-
-
-
 
